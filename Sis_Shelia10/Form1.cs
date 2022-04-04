e95db1efc0d5e641ca03ddc3e22f2f367ce3dd71
Gruzin4ik <102985337+Gruzin4ik@users.noreply.github.com>
2022-04-04 19:44:12 +0300

    Add files via upload

---
+++ Sis_Shelia10/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sis_Shelia10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] astrLogicalDrives = System.IO.Directory.GetLogicalDrives();
+            foreach (string disk in astrLogicalDrives)
+                listBox1.Items.Add(disk);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] astrLogicalDrives = System.Environment.GetLogicalDrives();
+            foreach (string disk in astrLogicalDrives)
+                listBox1.Items.Add(disk);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            System.IO.DriveInfo drv = new System.IO.DriveInfo(@"d:\");
+            listBox1.Items.Clear();
+            listBox1.Items.Add("Диск: " + drv.Name);
+
+            if (drv.IsReady)
+            {
+                listBox1.Items.Add("Тип диска: " + drv.DriveType.ToString());
+                listBox1.Items.Add("Файловая система: " +
+                drv.DriveFormat.ToString());
+                listBox1.Items.Add("Свободное место на диске: " +
+                drv.AvailableFreeSpace.ToString());
+            }
+    }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            listBox1.Items.Clear();
+            string[] astrFolders = System.IO.Directory.GetDirectories(@"d:\");
+            foreach (string folder in astrFolders)
+                listBox1.Items.Add(folder);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+           
+            listBox1.Items.Clear();
+            System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(@"d:\");
+            System.IO.DirectoryInfo[] folders = di.GetDirectories("*pro*");
+            foreach (System.IO.DirectoryInfo maskdirs in folders)
+                listBox1.Items.Add(maskdirs);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (System.IO.Directory.Exists(@"C:\windows"))
+                listBox1.Items.Add("Папка " + @"C:\Windows" + " существует");
+            else
+                listBox1.Items.Add("Папка не существует");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            
+            string oldPathString = @"C:\FERRARI";
+            string newPathString = @"C:\MERCEDES";
+            Directory.Move(oldPathString, newPathString);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                System.IO.Directory.Delete(@"c:\wutemp");
+                listBox1.Items.Add("Папка удалена.");
+            }
+            catch (Exception)
+            {
+                listBox1.Items.Add("Нельзя удалить непустую папку.");
+            }
+            finally { }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            
+            fbd.RootFolder = Environment.SpecialFolder.MyComputer;
+            
+            fbd.Description = "Выберите папку";
+            
+            fbd.ShowNewFolderButton = false;
+            
+            if (fbd.ShowDialog() == DialogResult.OK)
+                this.Text = fbd.SelectedPath;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            
+            MessageBox.Show(Environment.GetFolderPath(
+             Environment.SpecialFolder.Personal));
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+           
+            System.IO.DirectoryInfo dir = new
+            System.IO.DirectoryInfo(@"c:\wutemp");
+            listBox1.Items.Clear();
+            listBox1.Items.Add("Проверка папки: " + dir.Name);
+            listBox1.Items.Add("Родительская папка: " + dir.Parent.Name);
+            listBox1.Items.Add("Папка существует: ");
+            listBox1.Items.Add(dir.Exists.ToString());
+            if (dir.Exists)
+            {
+                listBox1.Items.Add("Папка создана: ");
+                listBox1.Items.Add(dir.CreationTime.ToString());
+                listBox1.Items.Add("Папка изменена: ");
+                listBox1.Items.Add(dir.LastWriteTime.ToString());
+                listBox1.Items.Add("Время последнего доступа: ");
+                listBox1.Items.Add(dir.LastAccessTime.ToString());
+                listBox1.Items.Add("Атрибуты папки: ");
+                listBox1.Items.Add(dir.Attributes.ToString());
+                listBox1.Items.Add("Папка содержит: " +
+                dir.GetFiles().Length.ToString() + " файла");
+            }
+        }
+    }
+    }
